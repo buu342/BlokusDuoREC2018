407bdd4c823e97c56535c9db03a8af6ad681a952
Buu342 <buu342@hotmail.com>
2018-02-09 16:49:17 +0000

    Rewrote debug_message

--- debug.h
+++ debug.h
@@ -1,7 +1,7 @@
 #ifndef ARDEBG_H
 #define ARDEBG_H
 
-void debug_message(char str[100]);
+void debug_message(char str[100], int variable, int ischar, int wait);
 
 void debug_draw_screen(int board[14][14]);
 
