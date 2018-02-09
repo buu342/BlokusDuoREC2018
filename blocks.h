d32bae0f2d22839ea6884532f47d3d901a86c5ee
Buu342 <buu342@hotmail.com>
2018-02-09 13:58:32 +0000

    Initial commit

---
+++ blocks.h
@@ -0,0 +1,37 @@
+#ifndef ARBLOK_H
+#define ARBLOK_H
+
+#define cr_red     "\x1b[31m"
+#define cr_green   "\x1b[32m"
+#define cr_yellow  "\x1b[33m"
+#define cr_blue    "\x1b[34m"
+#define cr_magenta "\x1b[35m"
+#define cr_cyan    "\x1b[36m"
+#define cr_reset   "\x1b[0m"
+
+#define MOVE_POSSIBLE   1
+#define MOVE_IMPOSSIBLE 0
+
+extern const int block_a[5][5];
+extern const int block_b[5][5];
+extern const int block_c[5][5];
+extern const int block_d[5][5];
+extern const int block_e[5][5];
+extern const int block_f[5][5];
+extern const int block_g[5][5];
+extern const int block_h[5][5];
+extern const int block_i[5][5];
+extern const int block_j[5][5];
+extern const int block_k[5][5];
+extern const int block_l[5][5];
+extern const int block_m[5][5];
+extern const int block_n[5][5];
+extern const int block_o[5][5];
+extern const int block_p[5][5];
+extern const int block_q[5][5];
+extern const int block_r[5][5];
+extern const int block_s[5][5];
+extern const int block_t[5][5];
+extern const int block_u[5][5];
+
+#endif
