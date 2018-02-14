eeb959d3c7beb68b5d47f01d32b2e9118586cf15
Buu342 <buu342@hotmail.com>
2018-02-14 14:15:08 +0000

    Created for makefile shortcut

---
+++ debug_empty.c
@@ -0,0 +1,15 @@
+#include "debug.h"
+
+/*------------------------------------
+  Debug functions       FINAL = YES
+------------------------------------*/
+
+void debug_message(char str[100], int variable, int ischar, int wait){};
+
+void debug_draw_screen(int board[14][14]){};
+
+void debug_enter(){};
+
+void debug_yield(int num, int move_x, int move_y, char piece, int rot, int score, int corners_left){};
+
+void debug_choice(int move_x, int move_y, char piece, int rot){};
