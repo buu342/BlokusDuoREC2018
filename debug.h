0e5135958bcd7a093d315ce658b9aa480f51a7c3
Buu342 <buu342@hotmail.com>
2018-02-14 14:00:31 +0000

    Created new debug functions

--- debug.h
+++ debug.h
@@ -7,4 +7,8 @@ void debug_draw_screen(int board[14][14]);
 
 void debug_enter();
 
+void debug_yield(int num, int move_x, int move_y, char piece, int rot, int score, int corners_left);
+
+void debug_choice(int move_x, int move_y, char piece, int rot);
+
 #endif
